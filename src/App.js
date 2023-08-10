e2209b52a81e1d3c94e0a4c5defbf0b8ef80b76c
midhulp <midhul@hamon.in>
2023-08-10 16:03:45 +0530

    Makes component gallery and use it in App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
-import {useState} from 'react';
-import { sculptureList } from './data.js';
+import Gallery from './gallery.js';
 
-export default function Gallery() {
-//   let index = 0;
-  const [index, setIndex]= useState(0);
-  const [showMore,setshowMore]= useState('');
-  
-
-  function handleClick() {
-    setIndex(index + 1);
-  }
-
-  function handleMoreClick(){
-    setshowMore(!showMore)
-  }
-
-  let sculpture = sculptureList[index];
+export default function Page() {
   return (
-    <>
-      <button onClick={handleClick}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i> 
-        by {sculpture.artist}
-      </h2>
-      <h3>  
-        ({index + 1} of {sculptureList.length})
-      </h3>
-     
-      <button onClick={handleMoreClick}>
-        {showMore ? 'hide' : 'show'} details
-
-      </button>
-      {showMore && <p> {sculpture.description} </p>}<br>
-      </br>
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
-      />
-    </>
+    <div className="Page">
+      <Gallery /><br></br>
+      <Gallery />
+    </div>
   );
 }
+
---
+++ src/gallery.js
@@ -0,0 +1,43 @@
+import {useState} from 'react';
+import { sculptureList } from './data.js';
+
+export default function Gallery() {
+  const [index, setIndex]= useState(0);
+  const [showMore,setshowMore]= useState('');
+  
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick(){
+    setshowMore(!showMore)
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <h2>
+        <i>{sculpture.name} </i> 
+        by {sculpture.artist}
+      </h2>
+      <h3>  
+        ({index + 1} of {sculptureList.length})
+      </h3>
+     
+      <button onClick={handleMoreClick}>
+        {showMore ? 'hide' : 'show'} details
+
+      </button>
+      {showMore && <p> {sculpture.description} </p>}<br>
+      </br>
+      <img 
+        src={sculpture.url} 
+        alt={sculpture.alt}
+      />
+    </>
+  );
+}
