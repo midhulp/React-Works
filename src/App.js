cc800b56d20d70ad335e33e95065ae645d9bb223
midhulp <midhul@hamon.in>
2023-08-10 15:56:48 +0530

    Adds feature to show and hide details

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import { sculptureList } from './data.js';
 export default function Gallery() {
 //   let index = 0;
   const [index, setIndex]= useState(0);
+  const [showMore,setshowMore]= useState('');
   
 
   function handleClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick(){
+    setshowMore(!showMore)
   }
 
   let sculpture = sculptureList[index];
@@ -23,13 +28,17 @@ export default function Gallery() {
       <h3>  
         ({index + 1} of {sculptureList.length})
       </h3>
+     
+      <button onClick={handleMoreClick}>
+        {showMore ? 'hide' : 'show'} details
+
+      </button>
+      {showMore && <p> {sculpture.description} </p>}<br>
+      </br>
       <img 
         src={sculpture.url} 
         alt={sculpture.alt}
       />
-      <p>
-        {sculpture.description}
-      </p>
     </>
   );
 }
