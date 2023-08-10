9d74216a7522dde3c96b641b1bedce54cf3c45a9
midhulp <midhul@hamon.in>
2023-08-10 15:45:09 +0530

    first program

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import {useState} from 'react';
 import { sculptureList } from './data.js';
 
 export default function Gallery() {
-  let index = 0;
+//   let index = 0;
+  const [index, setIndex]= useState(0);
+  
 
   function handleClick() {
-    index = index + 1;
+    setIndex(index + 1);
   }
 
   let sculpture = sculptureList[index];
