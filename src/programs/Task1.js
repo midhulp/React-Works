5ebdaf0649492a3b0f27a4804c6a895e00b16b84
midhulp <midhul@hamon.in>
2023-08-18 15:34:51 +0530

    Todo adds task

--- src/programs/Task1.js
+++ src/programs/Task1.js
@@ -4,24 +4,26 @@ import { useState } from "react";
 export default function Task(){
     const [todolist,setTodolist]=useState([]);
     const [newtask,setNewtask]=useState("");
-    const [addtask,setAddtask]=useState([]);
 
-    const handleOnchange =(e)=>{
-        setNewtask(e.target.value);
+    const handleOnchange =(e)=>{        setNewtask(e.target.value);
     };
     const addTask =()=>{
-        const newTodolist=[...todolist,newtask];
-        setTodolist(newTodolist)
-    }
-    return (
+        setTodolist([...todolist,newtask])
+    };
+    const deleteTask =(taskname)=>{
+        const newTodoList=todolist.filter((task)=>task !== taskname);
+        setTodolist(newTodoList);
+    };              
+        return (
     <div className="App">
     <div className="addTask">
         <input onChange={handleOnchange}/>
     <button onClick={addTask}>Task</button>
     </div>
     <div className="list">
-        {todolist.map((t)=>{
-            return <h1>{t}</h1>;
+        {todolist.map((task)=>{
+            return <div><h1>{task}</h1>
+             <button onClick={()=>deleteTask(task)}>x</button></div>;
         })}
     </div>
     </div>
