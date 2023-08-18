89dc650636fbeef57ee7d3209083007171fdefa9
midhulp <midhul@hamon.in>
2023-08-18 11:57:32 +0530

    Adds onchange example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Gallery from './gallery.js';
-import State_ex1 from './/programs/state_ex1.js'
+import Onchange_ex1 from './/programs/onchange_ex1.js'
 
 export default function Page() {
   return (
     <div className="Page">
-      <State_ex1/><br></br>
+      <Onchange_ex1/><br></br>
       
     </div>
   );
---
+++ src/programs/onchange_ex1.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function Change(){
+    const [value,setValue]= useState("")
+
+    const changevalue = (event)=>{
+        setValue(event
+            .target.value);
+    }
+
+
+
+    return(
+        <div className="App">
+            <input type="text" onChange={changevalue}/>
+              <div style={{background:'yellow'}}>{value}</div>  
+            
+        </div>
+
+    );
+}
