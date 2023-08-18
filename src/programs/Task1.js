b79ce6ce54371f8ae873019bbeeb4db1744a10d0
midhulp <midhul@hamon.in>
2023-08-18 16:35:09 +0530

    Adds todo features

--- src/programs/Task1.js
+++ src/programs/Task1.js
@@ -4,26 +4,33 @@ import { useState } from "react";
 export default function Task(){
     const [todolist,setTodolist]=useState([]);
     const [newtask,setNewtask]=useState("");
+    const [text,setText]=useState("");
 
     const handleOnchange =(e)=>{        setNewtask(e.target.value);
     };
     const addTask =()=>{
-        setTodolist([...todolist,newtask])
+        const task ={
+            id: todolist.length === 0 ? 1 : todolist[todolist.length-1].id + 1,
+            taskName: newtask,
+        };
+        setTodolist([...todolist,task])
+        setText(" ")
     };
-    const deleteTask =(taskname)=>{
-        const newTodoList=todolist.filter((task)=>task !== taskname);
+    const deleteTask =(id)=>{
+        const newTodoList=todolist.filter((task)=>task.id !== id);
         setTodolist(newTodoList);
-    };              
+    };       
         return (
     <div className="App">
     <div className="addTask">
-        <input onChange={handleOnchange}/>
+        <input onChange={handleOnchange} value={setText}/>
     <button onClick={addTask}>Task</button>
     </div>
     <div className="list">
         {todolist.map((task)=>{
-            return <div><h1>{task}</h1>
-             <button onClick={()=>deleteTask(task)}>x</button></div>;
+            return <div><h1>{task.taskName}</h1>
+             <button onClick={()=>deleteTask(task.id)}>x</button></div>;
+             
         })}
     </div>
     </div>
