304114026089a1c9aefc5c908138152d0e6d9997
midhulp <midhul@hamon.in>
2023-09-25 14:51:30 +0530

    Adds complete button

--- src/programs/Task1.js
+++ src/programs/Task1.js
@@ -1,51 +1,67 @@
 import { useState } from "react";
-import {Task1} from "./Task1prps"
+import { Task1 } from "./Task1prps"
 
 
-export default function Task(){
-    const [todolist,setTodolist]=useState([]);
-    const [newtask,setNewtask]=useState("");
+export default function Task() {
+    const [todolist, setTodolist] = useState([]);
+    const [newtask, setNewtask] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
-    
 
 
-    const handleOnchange =(event)=>{        
+
+    const handleOnchange = (event) => {
         setNewtask(event.target.value);
         setErrorMessage("")
     };
-    
-    const addTask =()=>{
-        if (newtask.trim() !== ""){
-        const task ={
-            id: todolist.length === 0 ? 1 : todolist[todolist.length-1].id + 1,
-            taskName: newtask,
-        };
-        setTodolist([...todolist,task])
-        setNewtask("")
-    }else {
-        alert("type something")
-        setErrorMessage("Type something");
-    }
+
+    const addTask = () => {
+        if (newtask.trim() !== "") {
+            const task = {
+                id: todolist.length === 0 ? 1 : todolist[todolist.length - 1].id + 1,
+                taskName: newtask,
+                completed: false,
+            };
+            setTodolist([...todolist, task])
+            setNewtask("")
+        } else {
+            alert("type something")
+            setErrorMessage("Type something");
+        }
     };
-    const deleteTask =(id)=>{
-        const newTodoList=todolist.filter((task)=>task.id !== id);
+    const deleteTask = (id) => {
+        const newTodoList = todolist.filter((task) => task.id !== id);
         setTodolist(newTodoList);
-        
-    };       
-    
-        return (
-    <div className="App">
-    <div className="addTask">
-        <input onChange={handleOnchange} value={newtask} placeholder="hello"/>
-    <button onClick={addTask}>Task</button>
-    <p style={{ color: "red" }}>{errorMessage}</p>
-    
-    </div>
-    <div className="list">
-        {todolist.map((task)=>{
-           return <Task1 taskName={task.taskName} id={task.id} deleteTask={deleteTask}/>;
-        })} 
-    </div>
-    </div>
+
+    };
+    const toggleCompleted = (id) => {
+        setTodolist(todolist.map((task) => {
+            if ((task.id === id)) {
+                return { ...task, completed: true };
+            } else {
+                return task;
+            }
+        })
+        );
+    };
+
+
+    return (
+        <div className="App">
+            <div className="addTask">
+                <input onChange={handleOnchange} value={newtask} placeholder="type here" />
+                <button onClick={addTask}>Task</button>
+                <p style={{ color: "red" }}>{errorMessage}</p>
+
+            </div>
+            <div className="list">
+                {todolist.map((task) => {
+                    return <Task1 taskName={task.taskName}
+                        id={task.id}
+                        completed={task.completed}
+                        deleteTask={deleteTask}
+                        toggleCompleted={toggleCompleted} />;
+                })}
+            </div>
+        </div>
     );
 }
--- src/programs/Task1prps.js
+++ src/programs/Task1prps.js
@@ -1,7 +1,11 @@
-export const Task1 = (props)=>{
+export const Task1 = (props) => {
 
-return (<div><h1>{props.taskName}</h1>
-             <button onClick={()=>props.deleteTask(props .id)}>x</button></div>
-             
-        );
-    };
+    return (<div className="task"
+        style={{ backgroundColor: props.completed ? "blue" : "red" }}>
+        <h1>{props.taskName}</h1>
+        <button onClick={() => props.deleteTask(props.id)}>x</button>
+        <button onClick={() => props.toggleCompleted(props.id)}>completed</button>
+    </div>
+
+    );
+};
