8ffc060f957b79988199e237e888f89de7633eea
midhulp <midhul@hamon.in>
2023-09-28 16:50:35 +0530

    Age quessing game

--- src/Ageapp/predictAge.js
+++ src/Ageapp/predictAge.js
@@ -3,17 +3,21 @@ import { useState } from "react"
 
 export default function Age() {
     const [name, setname] = useState("");
-    const [age, setage] = useState("")
+    const [age, setage] = useState({})
     const fetchname = () => {
         Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-            setage(res.data.age)
+            setage(res.data)
         })
     };
+
     return (
         <div className="App">
             <input onChange={(event) => setname(event.target.value)} />
             <button onClick={fetchname}>Quess Age</button>
-            <h1>{age && `AGE is ${age}`}</h1>
+            {age.age && <><h1>age:{age?.age}</h1>
+                <h1>count:{age?.count}</h1>
+                <h1>name:{age?.name}</h1></>}
+
         </div>
     )
 }
