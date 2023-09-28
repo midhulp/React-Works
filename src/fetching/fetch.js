a8eacf327fb4e69cc510fa35d5391a87038b083e
midhulp <midhul@hamon.in>
2023-09-28 15:18:19 +0530

    Adds Axios library

--- src/fetching/fetch.js
+++ src/fetching/fetch.js
@@ -1,8 +1,12 @@
-
+import Axios from "axios";
 
 export default function Cat() {
-    fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data) => {
-        console.log(data);
+    // fetch("https://catfact.ninja/fact").then((res) => res.json()).then((data) => {
+    //     console.log(data);
+    // });
+
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+        console.log(res.data);
     });
     return (
         <div className="App">
