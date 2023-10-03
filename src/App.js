e028414f048d827b414fec8729867623c7010ce2
midhulp <midhul@hamon.in>
2023-10-03 13:57:19 +0530

    Adds funny excuse api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Task1 from './/programs/Task1.js';
 import App from './/effects/effect.js';
 import Cat from './fetching/fetch.js';
 import Age from './Ageapp/predictAge.js';
+import Excuse from './excuse/excuseapp.js';
 
 export default function Page() {
   return (
     <div className="Page">
-      <Age /><br></br>
+      <Excuse /><br></br>
 
     </div>
   );
---
+++ src/excuse/excuseapp.js
@@ -0,0 +1,38 @@
+import Axios from "axios";
+import { useState } from "react"
+
+export default function Excuse() {
+    const [age, setage] = useState([{}])
+    const fetchcollege = () => {
+        Axios.get(`https://excuser-three.vercel.app/v1/excuse/party/`).then((res) => {
+            setage(res.data)
+        })
+    };
+
+    const fetchparty = () => {
+        Axios.get(`https://excuser-three.vercel.app/v1/excuse/party/`).then((res) => {
+            setage(res.data)
+        })
+    };
+
+    const fetchoffice = () => {
+        Axios.get(`https://excuser-three.vercel.app/v1/excuse/party/`).then((res) => {
+            setage(res.data)
+        })
+    };
+
+    console.log(age[0].excuse)
+
+    return (
+        <div className="App">
+            <button onClick={fetchcollege}>college</button>
+            <button onClick={fetchparty}>party</button>
+            <button onClick={fetchoffice}>office</button>
+            {age[0].excuse && <>
+                <h1>Excuse:{age[0]?.excuse}</h1>
+                <h1>Id:{age[0]?.id}</h1>
+                <h1>category:{age[0]?.category}</h1></>}
+
+        </div>
+    )
+}
